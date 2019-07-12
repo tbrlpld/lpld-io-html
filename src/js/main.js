7a95fbfa5201215780f26a4455638fc8d1a043c7
Tibor Leupold <tibor@lpld.io>
2019-07-12 15:47:35 +0200

    Created simple toggling for caejd project detail
    The toggling function can probably be made more generic to make it more
    reusable.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,28 @@
 $(main);
 
-function main(){
+function main() {
+  console.log(getLastWord('this-is-a-test'));
+  console.log('main is running');
+  $('#project-card-caejd').click(toggleProjectOverviewAndDetail);
+
+  $('#row-project-detail-caejd .close').click(() => {
+    console.log('Close button clicked');
+    $('#row-projects').toggle();
+    $('#row-project-detail-caejd').toggle();
+  });
+}
+
+function toggleProjectOverviewAndDetail() {
+  console.log('Clicked of project overview card');
+  let projIdent = getLastWord($(this).attr('id'));
+  console.log(projIdent);
+  let projDetailId = '#row-project-detail-' + projIdent;
+  console.log(projDetailId);
+  $('#row-projects').toggle();
+  $(projDetailId).toggle();
+};
+
+function getLastWord(dashedString) {
+  const words = dashedString.split('-');
+  return words[words.length - 1];
 }
